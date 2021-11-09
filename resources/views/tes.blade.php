c2fe062a4e5cb3e4640646e24747cbf2bab1e5a2
LAPTOP-J8GL49Q7\LENOVO <islamianurjanna05@gmail.com>
2021-11-09 12:30:31 +0800

    MidKakas_NurIslamiaTuljanna

---
+++ resources/views/tes.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4><b>Menurut Atlassian, version control system (VCS) adalah sebuah kumpulan perangkat lunak yang sudah terintegrasi dan digunakan untuk membantu software engineer mengelola perubahan dalam source code dari waktu ke waktu. </b></h4>
+    <h5>Jenis-jenis VCS</h5>
+    <ul><li>Bazaar</li>
+    <li>Sub Version</li>
+    <li>Mercurial</li>
+    <li>Revision Control System</li>
+    <li>ClearCase</li></ul>
+</body>
+</html>
